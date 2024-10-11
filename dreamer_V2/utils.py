2180c790c1a5eaa74f6aceb53fa847f5e70781d6
JoJo0217 <mingu0217@gmail.com>
2024-10-12 00:09:01 +0900

    fix critic loss

--- dreamer_V2/utils.py
+++ dreamer_V2/utils.py
@@ -233,24 +233,28 @@ def train_actor_critic(args, states, deters, world_model, actor, critic, target_
 
     predicted_rewards = reward(imagine_states, imagine_deters).mean.squeeze(-1)
     target_values = target_net(imagine_states, imagine_deters).squeeze(-1)
-    values = critic(imagine_states, imagine_deters).squeeze(-1)
+    values = critic(imagine_states.detach(), imagine_deters.detach()).squeeze(-1)
     discount_pred = discount(imagine_states, imagine_deters).mean.squeeze(-1)
 
     lambda_return_ = lambda_return(
         predicted_rewards, target_values, discount_pred, args.lambda_)
+
+    print(lambda_return_.mean(dim=-1), values.mean(dim=-1))
+
     critic_loss = nn.functional.mse_loss(values[:-1], lambda_return_[:-1].detach())
 
-    actor_loss = -args.reinforce_coef * (imagine_action_log_probs[:-1] * (lambda_return_[:-1] - values[:-1]).detach()).mean() -\
+    actor_loss = -args.reinforce_coef * (imagine_action_log_probs[:-1] * (lambda_return_[:-1] - target_values[:-1]).detach()).mean() -\
         (1 - args.reinforce_coef) * lambda_return_[:-1].mean() -\
         args.entropy_coef * imagine_entropy[:-1].mean()
 
     actor_optim.zero_grad()
-    critic_optim.zero_grad()
-    actor_loss.backward(retain_graph=True)
-    critic_loss.backward()
+    actor_loss.backward()
     nn.utils.clip_grad_norm_(actor.parameters(), args.clip_grad)
-    nn.utils.clip_grad_norm_(critic.parameters(), args.clip_grad)
     actor_optim.step()
+
+    critic_optim.zero_grad()
+    critic_loss.backward()
+    nn.utils.clip_grad_norm_(critic.parameters(), args.clip_grad)
     critic_optim.step()
 
     loss = {"actor_loss": actor_loss.item(), "critic_loss": critic_loss.item()}
